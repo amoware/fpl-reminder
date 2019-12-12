f1af72349a16a26830b3825a4f2804f8a48b21a3
jonazeiselt <jonazeiselt@gmail.com>
2019-12-12 20:23:22 +0100

    skapat klasser för Time och NotificationSettings

---
+++ app/src/main/java/com/amoware/fplreminder/NotificationSettings.java
@@ -0,0 +1,62 @@
+package com.amoware.fplreminder;
+
+import java.util.Date;
+
+/**
+ * Created by amoware on 2019-12-12.
+ */
+public class NotificationSettings {
+    private Date notificationTime;
+    private Date deadLineTime;
+    private Time scrollTime;
+    private boolean notificationSound;
+    private boolean notificationVibration;
+
+    public Date getNotificationTime() {
+        return notificationTime;
+    }
+
+    public void setNotificationTime(Date notificationTime) {
+        this.notificationTime = notificationTime;
+    }
+
+    public Date getDeadLineTime() {
+        return deadLineTime;
+    }
+
+    public void setDeadLineTime(Date deadLineTime) {
+        this.deadLineTime = deadLineTime;
+    }
+
+    public Time getScrollTime() {
+        return scrollTime;
+    }
+
+    public void setScrollTime(Time scrollTime) {
+        this.scrollTime = scrollTime;
+    }
+
+    public boolean isNotificationSound() {
+        return notificationSound;
+    }
+
+    public void setNotificationSound(boolean notificationSound) {
+        this.notificationSound = notificationSound;
+    }
+
+    public boolean isNotificationVibration() {
+        return notificationVibration;
+    }
+
+    public void setNotificationVibration(boolean notificationVibration) {
+        this.notificationVibration = notificationVibration;
+    }
+
+    /**
+     * Calculates the date for the notification to be set. The date depends on the deadline of a
+     * particular gameweek and a desired time before the deadline.
+     */
+    public Date calcNotificationTime(Date deadLineTime, Time scrollTime) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/amoware/fplreminder/Time.java
@@ -0,0 +1,25 @@
+package com.amoware.fplreminder;
+
+/**
+ * Created by amoware on 2019-12-12.
+ */
+public class Time {
+    private int hours;
+    private int minutes;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+}
