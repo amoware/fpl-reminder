13ae08ffaa1847a0e52ece91348c2236b620d900
jonazeiselt <jonazeiselt@gmail.com>
2019-12-29 13:33:13 +0100

    skapat klass för GameweekClient

---
+++ app/src/main/java/com/amoware/fplreminder/GameweekClient.java
@@ -0,0 +1,19 @@
+package com.amoware.fplreminder;
+
+import java.util.List;
+
+/**
+ * Created by amoware on 2019-12-29.
+ */
+public class GameweekClient {
+    private static final String API_DATA = "https://fantasy.premierleague.com/api/bootstrap-static/";
+    private HttpClient httpClient;
+
+    public List<Gameweek> getGameweeksFromFPL() {
+        return null;
+    }
+
+    private List<Gameweek> stringToListConverter(String gameweeksString) {
+        return null;
+    }
+}
