870c4fd00d26d18e66c8f44193140d99753d618f
ErikAkerman <erikakerman@gmail.com>
2019-12-29 13:55:36 +0100

    lagt till GameweekManager klassen

--- app/src/main/java/com/amoware/fplreminder/GameweekManager.java
+++ app/src/main/java/com/amoware/fplreminder/GameweekManager.java
@@ -1,4 +1,29 @@
 package com.amoware.fplreminder;
 
+import java.util.List;
+
 public class GameweekManager {
+
+    private Gameweek currentGaneweek;
+    private List Gameweek;
+    private GameweekClient gameweekClient;
+
+
+    public static void Initialize(){
+    }
+
+    public Gameweek getCurrentGaneweek() {
+        return currentGaneweek;
+    }
+
+    public static void refreshGameweek(){
+    }
+
+    public List readGameweeksFromDatabase(){
+        return Gameweek;
+    }
+
+    public static void saveGameweeksToDatabase(Gameweek gameweeks){
+    }
 }
+
