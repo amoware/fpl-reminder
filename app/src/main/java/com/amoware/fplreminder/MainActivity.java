aff48881dde3bbdb0c9de7d58e82428dad6bf5b9
ErikAkerman <erikakerman@gmail.com>
2019-12-29 15:12:30 +0100

    MainActivity skelett tillagd

--- app/src/main/java/com/amoware/fplreminder/MainActivity.java
+++ app/src/main/java/com/amoware/fplreminder/MainActivity.java
@@ -4,7 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Created by amoware on 2019-12-29.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationSettings notificationSettings;
+    private GameweekManager gameweekManager;
+
+    public void displayNotificationTimer(){
+    }
+
+    public void displayCountdownTimer(){
+    }
+
+    public void displayCurrentGameweek(){
+    }
+
+    public void displayNotificationPreferences(){
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
