604863ac895a8b182a9725a2600f905e3cb7310f
jonazeiselt <jonazeiselt@gmail.com>
2019-12-31 17:27:28 +0100

    uppdaterat kommentarer

--- app/src/main/java/com/amoware/fplreminder/alarm/ReminderReceiver.java
+++ app/src/main/java/com/amoware/fplreminder/alarm/ReminderReceiver.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.util.Log;
 
 /**
- * Is used to show the user a notification and the onReceive method is called when the time of the
+ * Is used to show the user a notification and the onReceive method is called when the time of an
  * alarm, that the user has set, occurs.
  * Created by amoware on 2019-12-30.
  */
