58857677a391e664f373c257e7353dfeb09d9a07
ErikAkerman <erikakerman@gmail.com>
2020-01-05 18:19:49 +0100

    HttpClient created

--- app/src/test/java/com/amoware/fplreminder/GetExample.java
+++ app/src/test/java/com/amoware/fplreminder/GetExample.java
@@ -1,32 +1,42 @@
 package com.amoware.fplreminder;
 
-import java.io.IOException;
-import java.net.URL;
+        import java.io.IOException;
+        import java.net.URL;
+        import java.util.concurrent.TimeUnit;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+        import okhttp3.MediaType;
+        import okhttp3.OkHttpClient;
+        import okhttp3.Request;
+        import okhttp3.RequestBody;
+        import okhttp3.Response;
 
 // GET an URL
 public class GetExample {
-    OkHttpClient client = new OkHttpClient();
+    public String url = "https://fantasy.premierleague.com/api/bootstrap-static/";
 
-    String run(String url) throws IOException {
+    public String run(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
+
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
-        try (Response response = client.newCall(request).execute()) {
+        Response response = client.newCall(request).execute();
+        System.out.println(response);
+        return response.body().string();
+
+        /*try (Response response = client.newCall(request).execute()) {
             return response.body().string();
-        }
+        }*/
     }
 
 
     public static void main(String[] args) throws IOException {
         GetExample example = new GetExample();
-        String response = example.run("https://raw.github.com/square/okhttp/master/README.md");
+        String response = example.run(example.url);
         System.out.println(response);
     }
 }
---
+++ app/src/test/java/com/amoware/fplreminder/JavaHttpUrlConnectionreader.java
@@ -0,0 +1,4 @@
+package com.amoware.fplreminder;
+
+public class JavaHttpUrlConnectionreader {
+}
