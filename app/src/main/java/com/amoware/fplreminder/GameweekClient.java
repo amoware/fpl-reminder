a854412449f533eb5b7a0e217ad6210202594b24
ErikAkerman <erikakerman@gmail.com>
2020-01-09 21:07:15 +0100

    gameweekclient started

--- app/src/main/java/com/amoware/fplreminder/GameweekClient.java
+++ app/src/main/java/com/amoware/fplreminder/GameweekClient.java
@@ -1,5 +1,7 @@
 package com.amoware.fplreminder;
 
+import org.json.JSONObject;
+
 import java.util.List;
 
 /**
@@ -10,7 +12,26 @@ public class GameweekClient {
     private HttpClient httpClient;
     private final String API_DATA = "https://fantasy.premierleague.com/api/bootstrap-static/";
 
-    public List<Gameweek> getGameweeksFromFPL() {
+    public static void main(String[] args) throws Exception {
+        // Skapa ett GameweekClient-objekt
+        getGameweeksFromFPL();
+    }
+
+    public List<Gameweek> getGameweeksFromFPL() throws Exception {
+        // Hämta api-datan
+        String apiString;
+        apiString = httpClient.sendGetRequest(API_DATA);
+
+        // Skapa en lista över gameweeks.
+        // 1. Gör om apiString till ett JSONObject
+        JSONObject jsonObject = new JSONObject(apiString);
+
+        //Tvivlar..
+        //jsonObject.getJSONArray("events");
+
+        // 2. Json-objektet har fältet "events" som består av en array (JSONArray) med gameweeks. Hämta ut JSONArray:en "events"
+
+
         return null;
     }
 
