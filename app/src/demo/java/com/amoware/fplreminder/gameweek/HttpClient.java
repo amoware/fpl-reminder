c11fda8a3e5608ce30a7adf2ef4295aa5e0558e9
jonazeiselt <jonaseiselt@hotmail.com>
2020-06-07 13:01:45 +0200

    uppdaterat httpclient

--- app/src/demo/java/com/amoware/fplreminder/gameweek/HttpClient.java
+++ app/src/demo/java/com/amoware/fplreminder/gameweek/HttpClient.java
@@ -8,6 +8,29 @@ public class HttpClient {
 
     @SuppressWarnings({"RedundantThrows", "unused"})
     public String sendGetRequest(String url) throws Exception {
-        return null;
+        return "{\n" +
+                "  \"events\": [\n" +
+                "    {\n" +
+                "      \"name\": \"Gameweek 1\",\n" +
+                "      \"deadline_time\": \"2020-06-07T12:50:00Z\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"Gameweek 2\",\n" +
+                "      \"deadline_time\": \"2020-06-07T12:53:00Z\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"Gameweek 3\",\n" +
+                "      \"deadline_time\": \"2020-06-07T12:55:00Z\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"Gameweek 4\",\n" +
+                "      \"deadline_time\": \"2020-06-07T12:57:00Z\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"Gameweek 5\",\n" +
+                "      \"deadline_time\": \"2020-06-07T12:59:00Z\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
     }
 }
