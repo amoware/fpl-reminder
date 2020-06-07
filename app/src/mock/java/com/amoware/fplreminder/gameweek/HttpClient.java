f2c588d0a5de0d64f00475d4689034b701b04913
jonazeiselt <jonaseiselt@hotmail.com>
2020-06-07 12:36:40 +0200

    vi mockar http-clienten

---
+++ app/src/mock/java/com/amoware/fplreminder/gameweek/HttpClient.java
@@ -0,0 +1,13 @@
+package com.amoware.fplreminder.gameweek;
+
+/**
+ * Created by amoware on 2020-06-07.
+ */
+@SuppressWarnings("WeakerAccess")
+public class HttpClient {
+
+    @SuppressWarnings({"RedundantThrows", "unused"})
+    public String sendGetRequest(String url) throws Exception {
+        return null;
+    }
+}
